e3037b8b593fc68fe23bfbae63adcb35d8e24fe6
Sami0189 <samisaimon28@gmail.com>
2024-10-28 22:38:14 +0600

    recap

---
+++ recap.js
@@ -0,0 +1,26 @@
+function add(price1,price2){
+    const total = price1 + price2;
+    return total;
+}
+ add(5,80);
+const bill = add(5,80);
+console.log(bill);
+
+
+
+function add2(price1,price2){
+    return price1 + price2;
+}add2(5,80);
+const bill1 =add2(5,80);
+console.log(bill1,bill)
+
+
+
+function doMath(num1,num2,num3){
+    const sum = num1 + num2 + num3;
+    const diff=
+    return total;
+}doMath(5,10,15);
+
+const all = doMath(5,10,15);
+console.log('the total amount is:',all);
