1e3088c9b4d9ef17de4f694c8443173235e2989c
Sami0189 <samisaimon28@gmail.com>
2024-10-28 20:22:54 +0600

    return

---
+++ return.js
@@ -0,0 +1,16 @@
+function tenTimes(number){
+     const result = number * 10;
+     return result;
+    
+}tenTimes(5);
+//ja kono nam daya jaba means sami,output etc.
+const sami =tenTimes(5);
+console.log('sami',sami);
+
+
+function cutHalf(number){
+    const fruits = number / 4;
+    return fruits;
+}cutHalf(10);
+const num = cutHalf(10);
+console.log('result',num);
