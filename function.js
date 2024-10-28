907c15a89afe8da0a692ed33894c20eeb39a34e8
Sami0189 <samisaimon28@gmail.com>
2024-10-28 19:26:34 +0600

    function and parameter

---
+++ function.js
@@ -0,0 +1,14 @@
+const number = 45;
+
+function fanoffKor(){
+    console.log('bosa thaka oda dara');
+    console.log('walk towards the switch');
+    console.log('click the switch to off of the fan');
+}
+fanoffKor();
+
+function brushTheTeeth(){
+    console.log('pick up the brush');
+    console.log('who are you');
+}
+brushTheTeeth();
---
+++ parameter.js
@@ -0,0 +1,25 @@
+
+
+function square(number){
+    console.log('value of th number parameter',number);
+    const borgo = number * number;
+    console.log('square of the given number is :',borgo);
+}
+
+square(4);
+
+square(12);
+square(3);
+
+function add(num1, num2){
+    const sum = num1 + num2;
+    console.log(sum);
+}
+add(3,5);
+add(43,76);
+add(423,53);
+
+function who(a,b,c,d,e){
+     const num =a + b + c + d + e;
+     console.log(num);
+}who(3,3,3,3,3);
