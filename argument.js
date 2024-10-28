6eb81ee179e68f373d8d08efa9488fe30fc8ac5f
Sami0189 <samisaimon28@gmail.com>
2024-10-28 19:52:59 +0600

    argument

---
+++ argument.js
@@ -0,0 +1,28 @@
+function doubleIt(number){
+    const doubled = number * 2;
+    console.log(number,doubled);
+}
+console.log('I will call the function');
+doubleIt(15);
+console.log('---------');
+doubleIt(88);
+console.log('---------');
+doubleIt(66);
+
+const number = 55;
+doubleIt(number);
+const money = 112;
+doubleIt(money);
+
+
+
+function difference(num1,num2){
+    const diff =num1 - num2;
+    console.log(num1,num2,'difference is',diff);
+}difference(4,4);
+
+fatherAge =50;
+myAge=20;
+difference(fatherAge,myAge);
+
+//difference ar bitor sonkha o hota para abar variable ar nam o hota para.
